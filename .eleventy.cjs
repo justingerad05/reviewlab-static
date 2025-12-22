4c6c6449337c46db4aa96c0c667f287103c8f3a1
justingerad05 <justingerad05@gmail.com>
2025-12-22 14:39:42 -0500

    Update .eleventy.cjs

--- .eleventy.cjs
+++ .eleventy.cjs
@@ -1,4 +1,11 @@
-module.exports = function () {
+module.exports = function (eleventyConfig) {
+  // Allow Eleventy to process XML templates
+  eleventyConfig.setTemplateFormats([
+    "html",
+    "njk",
+    "xml"
+  ]);
+
   return {
     dir: {
       input: ".",
