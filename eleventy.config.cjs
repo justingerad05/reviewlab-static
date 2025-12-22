a1fc1e87d0dfe3e3db58cafefed05272af4d2215
justingerad05 <justingerad05@gmail.com>
2025-12-22 12:58:49 -0500

    Update eleventy.config.cjs

--- eleventy.config.cjs
+++ eleventy.config.cjs
@@ -1,21 +1,10 @@
-module.exports = function (eleventyConfig) {
-
-  eleventyConfig.addLiquidFilter("extractYoutubeThumbnail", function (content) {
-    if (!content || typeof content !== "string") return "";
-
-    const match = content.match(
-      /(youtube\.com\/embed\/|youtube\.com\/watch\?v=|youtu\.be\/)([a-zA-Z0-9_-]{11})/
-    );
-
-    if (!match) return "";
-
-    return "https://img.youtube.com/vi/" + match[2] + "/maxresdefault.jpg";
-  });
-
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("posts");
   return {
     dir: {
       input: ".",
-      layouts: "_layouts"
+      includes: ".",
+      output: "_site"
     }
   };
 };
