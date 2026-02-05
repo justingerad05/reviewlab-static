e8d75ce05e116917a481d2ab6e7d4f4dbac3468b
justingerad05 <justingerad05@gmail.com>
2026-02-05 14:03:39 +0100

    Update .eleventy.cjs

--- .eleventy.cjs
+++ .eleventy.cjs
@@ -1,7 +1,17 @@
 module.exports = function (eleventyConfig) {
+
+  /* PASS THROUGH STATIC FILES */
+  eleventyConfig.addPassthroughCopy("og-default.jpg");
+  eleventyConfig.addPassthroughCopy("og-images");
+  eleventyConfig.addPassthroughCopy("robots.txt");
+
+  /* WATCH FOR OG CHANGES */
+  eleventyConfig.addWatchTarget("./og-images/");
+
   eleventyConfig.setTemplateFormats([
     "html",
     "njk",
+    "liquid",
     "xml"
   ]);
 
