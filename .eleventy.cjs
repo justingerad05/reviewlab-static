d9954cd02209c484f2618f528abfb609772b38b6
justingerad05 <justingerad05@gmail.com>
2026-02-06 20:09:09 +0100

    Update .eleventy.cjs

--- .eleventy.cjs
+++ .eleventy.cjs
@@ -7,6 +7,9 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("og-cta-features.jpg");
   eleventyConfig.addPassthroughCopy("og-cta-tested.jpg");
   eleventyConfig.addPassthroughCopy("og-cta-verdict.jpg");
+  eleventyConfig.addPassthroughCopy("sitemap.xml");
+  eleventyConfig.addPassthroughCopy("tags");
+
 
   /* ✅ BULLETPROOF POSTS COLLECTION */
   eleventyConfig.addCollection("posts", function(collectionApi) {
