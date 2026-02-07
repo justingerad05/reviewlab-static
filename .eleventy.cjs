b28df1374911b3a6ecdc5121471b8c8ae2d3cc64
justingerad05 <justingerad05@gmail.com>
2026-02-07 13:48:06 +0100

    Update .eleventy.cjs

--- .eleventy.cjs
+++ .eleventy.cjs
@@ -2,14 +2,9 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy("og-images");
   eleventyConfig.addPassthroughCopy("*.jpg");
+  eleventyConfig.addPassthroughCopy("sitemap.xml");
 
   eleventyConfig.addWatchTarget("./og-images/");
-
-  eleventyConfig.addCollection("posts", function(collectionApi) {
-    return collectionApi
-      .getFilteredByGlob("./posts/**/*.html")
-      .sort((a, b) => b.date - a.date);
-  });
 
   return {
     dir:{
