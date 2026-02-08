2d429d72351be9571fbdcd7ccf4cbbe5623535bc
justingerad05 <justingerad05@gmail.com>
2026-02-08 01:29:31 +0100

    Update .eleventy.cjs

--- .eleventy.cjs
+++ .eleventy.cjs
@@ -1,11 +1,8 @@
 module.exports = function(eleventyConfig) {
-
   eleventyConfig.addPassthroughCopy("og-images");
   eleventyConfig.addPassthroughCopy("*.jpg");
   eleventyConfig.addPassthroughCopy("sitemap.xml");
-
   eleventyConfig.addWatchTarget("./og-images/");
-
   return {
     dir:{
       input:".",
