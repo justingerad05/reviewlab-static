c3fe8a24428618eca2c88e18c3177a2110a79ad0
justingerad05 <justingerad05@gmail.com>
2026-02-10 21:19:37 +0100

    Update .eleventy.cjs

--- .eleventy.cjs
+++ .eleventy.cjs
@@ -1,10 +1,14 @@
 module.exports = function(eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy("og-images");
+  eleventyConfig.addPassthroughCopy("topics");     // ✅ FIX 404
+  eleventyConfig.addPassthroughCopy("author");     // ✅ ensure deploy
   eleventyConfig.addPassthroughCopy("*.jpg");
   eleventyConfig.addPassthroughCopy("sitemap.xml");
 
   eleventyConfig.addWatchTarget("./og-images/");
+  eleventyConfig.addWatchTarget("./topics/");
+  eleventyConfig.addWatchTarget("./author/");
 
   return {
     dir:{
