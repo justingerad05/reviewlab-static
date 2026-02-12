e6528fe16653b72e200eaf5f8685f560f7d25928
justingerad05 <justingerad05@gmail.com>
2026-02-12 12:10:34 +0100

    Update .eleventy.cjs

--- .eleventy.cjs
+++ .eleventy.cjs
@@ -1,8 +1,12 @@
+const htmlmin = require("@11ty/eleventy-plugin-htmlmin");
+
 module.exports = function(eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy("og-images");
-  eleventyConfig.addPassthroughCopy("topics");     // ✅ FIX 404
-  eleventyConfig.addPassthroughCopy("author");     // ✅ ensure deploy
+  eleventyConfig.addPassthroughCopy("topics");
+  eleventyConfig.addPassthroughCopy("author");
+  eleventyConfig.addPassthroughCopy("editorial-policy");
+  eleventyConfig.addPassthroughCopy("review-methodology");
   eleventyConfig.addPassthroughCopy("*.jpg");
   eleventyConfig.addPassthroughCopy("sitemap.xml");
 
@@ -10,13 +14,11 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addWatchTarget("./topics/");
   eleventyConfig.addWatchTarget("./author/");
 
-  const htmlmin = require("@11ty/eleventy-plugin-htmlmin");
-
   eleventyConfig.addPlugin(htmlmin,{
-  collapseWhitespace:true,
-  removeComments:true,
-  useShortDoctype:true
-});
+    collapseWhitespace:true,
+    removeComments:true,
+    useShortDoctype:true
+  });
 
   return {
     dir:{
